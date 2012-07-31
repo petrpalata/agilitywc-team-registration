0274a27430b0d76e787e6c404e7beccaead3b423
Petr Palata <petrpalata@Joes-Mac-mini.local>
2012-07-31 21:12:16 +0200

    Routes

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ gem 'jquery-rails'
 
 # Deploy with Capistrano
 gem 'capistrano'
+gem 'rvm-capistrano'
 
 # To use debugger
 # gem 'ruby-debug'
--- config/routes.rb
+++ config/routes.rb
@@ -1,76 +1,77 @@
 TeamRegistration::Application.routes.draw do
+    scope '/team_registration' do
+        root :to => "index#index"
 
-  root :to => "index#index"
+        devise_for :users, :skip => ['sessions', 'registrations']
 
-  devise_for :users, :skip => ['sessions', 'registrations']
+        devise_scope :user do 
+            get "/new_user" => "registration#new", :as => 'new_user_registration'
+            post "/new_user" => "registration#create", :as => 'create_user_registration'
 
-  devise_scope :user do 
-      get "/new_user" => "registration#new", :as => 'new_user_registration'
-      post "/new_user" => "registration#create", :as => 'create_user_registration'
+            get "/login" => "devise/sessions#new", :as => 'new_user_session'
+            post "/login" => "devise/sessions#create", :as => 'user_session'
+            delete "/logout" => "devise/sessions#destroy", :as => 'destroy_user_session'
+        end
 
-      get "/login" => "devise/sessions#new", :as => 'new_user_session'
-      post "/login" => "devise/sessions#create", :as => 'user_session'
-      delete "/logout" => "devise/sessions#destroy", :as => 'destroy_user_session'
-  end
+        resources :teams, :as => :handlers
 
-  resources :teams, :as => :handlers
+        match "/confirmation" => "confirmation#index"
+        match "/confirmation/confirm_all" => "confirmation#confirm_all"
+        match "/confirmation/payment_information" => "confirmation#payment_information"
+    end
+    # The priority is based upon order of creation:
+    # first created -> highest priority.
 
-  match "/confirmation" => "confirmation#index"
-  match "/confirmation/confirm_all" => "confirmation#confirm_all"
-  match "/confirmation/payment_information" => "confirmation#payment_information"
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
+    # Sample of regular route:
+    #   match 'products/:id' => 'catalog#view'
+    # Keep in mind you can assign values other than :controller and :action
 
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
+    # Sample of named route:
+    #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
+    # This route can be invoked with purchase_url(:id => product.id)
 
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
+    # Sample resource route (maps HTTP verbs to controller actions automatically):
+    #   resources :products
 
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+    # Sample resource route with options:
+    #   resources :products do
+    #     member do
+    #       get 'short'
+    #       post 'toggle'
+    #     end
+    #
+    #     collection do
+    #       get 'sold'
+    #     end
+    #   end
 
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+    # Sample resource route with sub-resources:
+    #   resources :products do
+    #     resources :comments, :sales
+    #     resource :seller
+    #   end
 
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+    # Sample resource route with more complex sub-resources
+    #   resources :products do
+    #     resources :comments
+    #     resources :sales do
+    #       get 'recent', :on => :collection
+    #     end
+    #   end
 
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
+    # Sample resource route within a namespace:
+    #   namespace :admin do
+    #     # Directs /admin/products/* to Admin::ProductsController
+    #     # (app/controllers/admin/products_controller.rb)
+    #     resources :products
+    #   end
 
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+    # You can have the root of your site routed with "root"
+    # just remember to delete public/index.html.
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
+    # See how all your routes lay out with "rake routes"
 
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+    # This is a legacy wild controller route that's not recommended for RESTful applications.
+    # Note: This route will make all actions in every controller accessible via GET requests.
+    # match ':controller(/:action(/:id))(.:format)'
 end
