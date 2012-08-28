3e4d0559e060a7b4c5f4a7422e5a757be0051cdb
Petr Palata <petr.palata@gmail.com>
2012-08-29 01:26:21 +0200

    Added thumbnail image to handlers

--- app/models/handler.rb
+++ app/models/handler.rb
@@ -2,7 +2,7 @@ class Handler < ActiveRecord::Base
   attr_accessible :country_id, :first_name, :insurance, :last_name, :dogs_attributes, :picture
   validates_presence_of :first_name, :last_name, :dogs, :country_id, :picture, :insurance
   has_many :dogs
-  has_attached_file :picture
+  has_attached_file :picture, :styles => { :thumbnail => '50x80' }
   validates_attachment_presence :picture
   validates_attachment_size :picture, :maximum => 5.megabytes
 
