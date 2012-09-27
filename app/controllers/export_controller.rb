a1d5775ed159f0888c4a8d92e0ed762007052fd0
Petr Palata <petr.palata@gmail.com>
2012-09-27 04:53:20 +0200

    Bugfix in starnumbers generation

--- app/controllers/export_controller.rb
+++ app/controllers/export_controller.rb
@@ -13,7 +13,7 @@ class ExportController < ApplicationController
     end
 
     def catalogue
-        @handlers = Handler.all(:include => :dogs, :order => "dogs.start_number")
+        @dogs = Dog.all(:include => :handlers, :order => "dogs.category DESC, dogs.start_number")
         respond_to do |format|
             format.csv {
                 render :partial => "export/catalogue.csv"
@@ -76,7 +76,7 @@ class ExportController < ApplicationController
     end
 
     def for_main_judge
-        @handlers = Handler.all(:include => :dogs, :order => "dogs.start_number")
+        @dogs = Dog.all(:include => :handlers, :order => "dogs.category DESC, dogs.start_number")
         respond_to do |format|
             format.csv {
                 render :partial => "export/catalogue_for_main_judge.csv"
