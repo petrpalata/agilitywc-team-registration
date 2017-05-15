da923f163dc31cc46858afadb9b3070da914fd7b
Petr <petr.palata@gmail.com>
2017-05-15 23:24:29 +0200

    Cap file fixing

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,17 @@
 require 'bundler/capistrano'
-require 'rvm/capistrano'
+require 'capistrano-rbenv'
+
 set :application, "team_registration"
 #set :repository, "ssh://petr@kacr.info:10522/home/git/repositories/team_registration.git"
-set :repo_url, 'git@bitbucket.org:petrpalata/team_registration.git'
+set :repository, 'git@bitbucket.org:petrpalata/team_registration.git'
 set :branch, "master"
 #set :deploy_via, :remote_cache
 set :scm, :git
-set :rvm_ruby_string, 'ree-1.8.7-2011.12'
-set :rvm_type, :system
 set :bundle_flags,    "--deployment"
 set :bundle_without,  []
 set :use_sudo, false
+
+set :rbenv_ruby_version, "1.9.3-p551"
 
 load 'deploy/assets'
 
@@ -27,6 +28,18 @@ role :db,  "agility2017.cz", :primary => true
 
 set :user, "kwertan"
 
+set :stages, ["production"]
+set :default_stage, "production"
+
+set :repository_cache, "git_cache"
+set :deploy_via, :remote_cache
+
+# Default value for :linked_files is []
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# Default value for linked_dirs is []
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system','config/database.yml', 'config/secrets.yml')
+
 namespace :passenger do
     desc "Restart Application"
     task :restart, :roles => :app, :except => { :no_release => true } do
@@ -42,15 +55,3 @@ namespace :deploy do
 end
 
 after :deploy, "passenger:restart"
-
-set :stages, ["production"]
-set :default_stage, "production"
-
-set :repository_cache, "git_cache"
-set :deploy_via, :remote_cache
-
-# Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
