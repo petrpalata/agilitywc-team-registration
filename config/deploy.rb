cb8e5c16e27249c3a6fa5ed303fd0763b8ce495e
Petr <petr.palata@gmail.com>
2017-05-15 23:42:32 +0200

    Ignoring database file

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,7 +38,7 @@ set :deploy_via, :remote_cache
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system','config/database.yml', 'config/secrets.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
 
 namespace :passenger do
     desc "Restart Application"
