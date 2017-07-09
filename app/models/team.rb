21477c3659d3a7d305f519e5f83351ef111b928b
Petr <petr.palata@gmail.com>
2017-07-09 22:27:27 +0200

    Fixed wrong picture URL in team/staff_member

--- app/models/team.rb
+++ app/models/team.rb
@@ -42,7 +42,7 @@ class Team < ActiveRecord::Base
 
 
   # picture
-  has_attached_file :picture, :styles => { :thumbnail => '50x80', :big_thumb => '120x120' }, :path => ":rails_root/public/system/:class/:attachment/:id/:style/:filename"
+  has_attached_file :picture, :styles => { :thumbnail => '50x80', :big_thumb => '120x120' }, :path => ":rails_root/public/system/:class/:attachment/:id/:style/:filename", :url => '/system/:class/:attachment/:id/:style/:filename'
 
   validates_attachment_presence :picture
   validates_attachment_size :picture, :less_than => 5.megabytes
